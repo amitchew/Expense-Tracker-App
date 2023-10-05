0ed4abf29b7ab99d48bd275af80adf4f76352264
amitchew <49229715+amitchew@users.noreply.github.com>
2023-10-05 18:09:55 +0300

    fix: added a new Expense form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ExpenseItems from "./components/ExpenseItem.js";
 import expenses from "./expenses";
-import './components/Expenses.css'
-import Card from "./components/Card";
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
+function App() {
   return (
-    <Card className="expenses">
-      <ExpenseItems
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItems
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-    </Card>
+    <>
+      <NewExpense />
+
+      <Expenses items={expenses} />
+    </>
   );
 }
 
